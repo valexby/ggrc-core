c36b10baea3167365499b5a1440327884342abe9
YuriChupyrkin <Yuri.Chupyrkin@gmail.com>
2019-11-20 11:15:48 +0300

    Add 'Assessment workflows' label on Assessment Template info-pane

--- src/ggrc-client/js/plugins/utils/verification-flow-utils.js
+++ src/ggrc-client/js/plugins/utils/verification-flow-utils.js
@@ -3,8 +3,6 @@
  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
  */
 
-import canMap from 'can-map';
-
 const VERIFICATION_FLOWS = {
   STANDARD: 'STANDARD',
   SOX_302: 'SOX302',
@@ -15,17 +13,21 @@ const getAssessmentFlows = () => {
   return Object.keys(VERIFICATION_FLOWS).map((key) => VERIFICATION_FLOWS[key]);
 };
 
-const isStandardFlow = (instance = canMap()) => {
+const isStandardFlow = (instance) => {
   return instance.attr('verification_workflow') === VERIFICATION_FLOWS.STANDARD;
 };
 
-const isSox302Flow = (instance = canMap()) => {
+const isSox302Flow = (instance) => {
   return instance.attr('verification_workflow') === VERIFICATION_FLOWS.SOX_302;
 };
 
-const isMultiLevelFlow = (instance = canMap()) => {
+const isMultiLevelFlow = (instance) => {
   return instance.attr('verification_workflow') ===
     VERIFICATION_FLOWS.MULTI_LEVEL;
+};
+
+const getFlowDisplayName = (instance) => {
+  return instance.attr('verification_workflow');
 };
 
 export {
@@ -33,4 +35,5 @@ export {
   isSox302Flow,
   isMultiLevelFlow,
   getAssessmentFlows,
+  getFlowDisplayName,
 };
