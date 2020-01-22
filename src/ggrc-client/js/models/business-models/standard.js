28a4a81d2ce92e73fd587eed8570a856942a0ce8
natalya-mihalyonok <natallia_mikhalenak@epam.com>
2020-01-22 17:35:01 +0300

    Add ChangeableExternally mixin to Regulation and Standard

--- src/ggrc-client/js/models/business-models/standard.js
+++ src/ggrc-client/js/models/business-models/standard.js
@@ -3,13 +3,15 @@
     Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 */
 
-import Directive from './directive';
+import Cacheable from '../cacheable';
 import AccessControlList from '../mixins/access-control-list';
 import Reviewable from '../mixins/reviewable';
+import ChangeableExternally from '../mixins/changeable-externally';
 
-export default Directive.extend({
+export default Cacheable.extend({
   root_object: 'standard',
   root_collection: 'standards',
+  category: 'governance',
   model_plural: 'Standards',
   table_plural: 'standards',
   title_plural: 'Standards',
@@ -23,21 +25,49 @@ export default Directive.extend({
   destroy: 'DELETE /api/standards/{id}',
   is_custom_attributable: true,
   isRoleable: true,
-  attributes: {},
   mixins: [
     AccessControlList,
     Reviewable,
+    ChangeableExternally,
   ],
+  tree_view_options: {
+    attr_list: Cacheable.attr_list.concat([
+      {
+        attr_title: 'State',
+        attr_name: 'status',
+        order: 40,
+      }, {
+        attr_title: 'Review State',
+        attr_name: 'review_status',
+        order: 80,
+      }, {
+        attr_title: 'Effective Date',
+        attr_name: 'start_date',
+        order: 85,
+      }, {
+        attr_title: 'Reference URL',
+        attr_name: 'reference_url',
+        order: 90,
+      }, {
+        attr_title: 'Description',
+        attr_name: 'description',
+        order: 95,
+      }, {
+        attr_title: 'Notes',
+        attr_name: 'notes',
+        order: 100,
+      }, {
+        attr_title: 'Assessment Procedure',
+        attr_name: 'test_plan',
+        order: 105,
+      }, {
+        attr_title: 'Last Deprecated Date',
+        attr_name: 'end_date',
+        order: 110,
+      }]),
+  },
   sub_tree_view_options: {
     default_filter: ['Requirement'],
   },
-  defaults: {
-    status: 'Draft',
-    kind: 'Standard',
-  },
   statuses: ['Draft', 'Deprecated', 'Active'],
-  init: function () {
-    Object.assign(this.attributes, Directive.attributes);
-    this._super(...arguments);
-  },
 }, {});
